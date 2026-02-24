3bdbc2ebc1ec399898213b2102eb38b890984a3e
zubednarova <zuzana.bednarova@keboola.com>
2026-02-24 19:23:07 +0100

    Update setup.sh

--- keboola-config/setup.sh
+++ keboola-config/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
 cd /app
-pip install -r requirements.txt --break-system-packages
+pip install -r requirements.txt
+echo "=== VENV LOCATION ==="
+find / -name "uvicorn" -type f 2>/dev/null
